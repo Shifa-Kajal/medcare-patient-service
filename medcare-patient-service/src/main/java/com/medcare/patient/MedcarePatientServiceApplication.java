870fe36685f8711f52a628b1404b3a5a63a58470
Shifa Kajal <Shifa.Kajal@fullstacklabs.digital>
2026-02-06 16:41:39 -0500

    Initial patient-service Spring Boot setup

---
+++ medcare-patient-service/src/main/java/com/medcare/patient/MedcarePatientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medcare.patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedcarePatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedcarePatientServiceApplication.class, args);
+	}
+
+}
---
+++ medcare-patient-service/src/test/java/com/medcare/patient/MedcarePatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medcare.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedcarePatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
